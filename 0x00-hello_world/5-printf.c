e88c0290c531d82e07d791402d41b1d046b8cf54
BlessedDavin <namara@agentofcode.com>
2022-03-14 17:30:06 -0700

    programing comit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(void){
+printf("%s", "with proper grammer, but the output is a piece of artwork,\n");
+return(0);
+}
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(void){
+printf("Size of a char:" %1d bytes\n", sizeof(char));
+printf("Size of an int:" %1d bytes\n", sizeof(int));
+printf("Size of a long int:" %1d bytes\n", sizeof(long int));
+printf("Size of a long long int:" %1d bytes\n", sizeof(long long int));
+printf("Size of a float:" %1d bytes\n", sizeof(float);
+return(0);
+}
